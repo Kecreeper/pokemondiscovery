e97f514630f91dfd06c6a7fa542dc90acb8d5410
Kecreeper <Kecreeper@gmail.com>
2024-08-12 13:51:50 -0400

    input

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,14 @@ import './App.css'
 
 const pokeUrl = "https://pokeapi.co/api/v2/pokemon/";
 
+function displayPokemonData() {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
 async function getPokemonData(input) {
   try {
     const response = await fetch(pokeUrl + input);
@@ -18,12 +26,19 @@ async function getPokemonData(input) {
 }
 
 function App() {
+  const [pokemonInput, setPokemonInput] = useState("charmander");
+
+  const handleChange = (event) => {
+    setPokemonInput(event.target.value);
+  }
+
   return (
     <div className='bg-white/75 mx-auto max-w-3xl rounded-xl grid grid-cols-1'>
+      
       <div className='text-2xl text-center py-4 font-sans font-bold'>
         Pokemon Discovery
       </div>
-      <input className='max-w-xl mx-auto'></input>
+      <input value={pokemonInput} onChange={handleChange} className='max-w-xl mx-auto rounded-md' />
       <div className='flex justify-around py-4'>
         <button className='bg-white py-1 px-6 rounded-lg border border-b-black border-r-black'>Query Pokemon</button>
         <button className='bg-white py-1 px-6 rounded-lg border border-b-black border-r-black'>Random Pokemon</button>
