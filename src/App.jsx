a93a4b01b9cc7d148a42e186c9bbeb8c9879422e
Kecreeper <Kecreeper@gmail.com>
2024-08-12 14:01:13 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,14 @@ import './App.css'
 
 const pokeUrl = "https://pokeapi.co/api/v2/pokemon/";
 
-function displayPokemonData() {
+function PokemonData({ data }) {
+  
+  
   return (
-    <div>
-
+    <div className='bg-white/75 mx-auto my-5 max-w-3xl rounded-xl grid grid-cols-1'>
+      <div>
+        asdfasdf
+      </div>
     </div>
   )
 }
@@ -27,24 +31,31 @@ async function getPokemonData(input) {
 
 function App() {
   const [pokemonInput, setPokemonInput] = useState("charmander");
+  const [data, setData] = useState(null);
 
   const handleChange = (event) => {
     setPokemonInput(event.target.value);
   }
 
+  const queryPokemon = async function () {
+
+  }
+
   return (
-    <div className='bg-white/75 mx-auto max-w-3xl rounded-xl grid grid-cols-1'>
-      
-      <div className='text-2xl text-center py-4 font-sans font-bold'>
-        Pokemon Discovery
+    <>
+      <PokemonData />
+      <div className='bg-white/75 mx-auto max-w-xl rounded-xl grid grid-cols-1'>
+        <div className='text-2xl text-center py-4 font-sans font-bold'>
+          Pokemon Discovery
+        </div>
+        <input value={pokemonInput} onChange={handleChange} className='max-w-xl mx-auto rounded-md' />
+        <div className='flex justify-around py-4'>
+          <button className='bg-white py-1 px-6 rounded-lg border border-b-black border-r-black'>Query Pokemon</button>
+          <button className='bg-white py-1 px-6 rounded-lg border border-b-black border-r-black'>Random Pokemon</button>
+        </div>
+        
       </div>
-      <input value={pokemonInput} onChange={handleChange} className='max-w-xl mx-auto rounded-md' />
-      <div className='flex justify-around py-4'>
-        <button className='bg-white py-1 px-6 rounded-lg border border-b-black border-r-black'>Query Pokemon</button>
-        <button className='bg-white py-1 px-6 rounded-lg border border-b-black border-r-black'>Random Pokemon</button>
-      </div>
-      
-    </div>
+    </>
   )
 }
 
